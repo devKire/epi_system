301d6b9c2cd3770aa54d98b16ce0a2a9a3c678b5
devKire <erikdossantos2006@outlook.com>
2025-12-06 08:55:59 -0300

    build

---
+++ prisma/migrations/20251206115511_initial_migration/migration.sql
@@ -0,0 +1,81 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'COLABORADOR');
+
+-- CreateEnum
+CREATE TYPE "EmprestimoStatus" AS ENUM ('EMPRESTADO', 'EM_USO', 'FORNECIDO', 'DEVOLVIDO', 'DANIFICADO', 'PERDIDO');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'COLABORADOR',
+    "colaboradorId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Colaborador" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "avatarUrl" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "matricula" TEXT NOT NULL,
+    "cargo" TEXT NOT NULL,
+    "ativo" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Colaborador_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "EPI" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "categoria" TEXT NOT NULL,
+    "quantidade" INTEGER NOT NULL,
+    "validade" TIMESTAMP(3),
+    "descricao" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "EPI_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "emprestimos" (
+    "id" TEXT NOT NULL,
+    "colaboradorId" TEXT NOT NULL,
+    "epiId" TEXT NOT NULL,
+    "quantidade" INTEGER NOT NULL,
+    "dataEmprestimo" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "dataDevolucao" TIMESTAMP(3),
+    "dataVencimento" TIMESTAMP(3) NOT NULL,
+    "status" "EmprestimoStatus" NOT NULL DEFAULT 'EMPRESTADO',
+    "observacao" TEXT,
+    "observacaoDevolucao" TEXT,
+
+    CONSTRAINT "emprestimos_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_colaboradorId_key" ON "User"("colaboradorId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Colaborador_email_key" ON "Colaborador"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Colaborador_matricula_key" ON "Colaborador"("matricula");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_colaboradorId_fkey" FOREIGN KEY ("colaboradorId") REFERENCES "Colaborador"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "emprestimos" ADD CONSTRAINT "emprestimos_colaboradorId_fkey" FOREIGN KEY ("colaboradorId") REFERENCES "Colaborador"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "emprestimos" ADD CONSTRAINT "emprestimos_epiId_fkey" FOREIGN KEY ("epiId") REFERENCES "EPI"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
