6526badca8007599dd95fe14fa3f92e87b640519
ERIK RAFAEL DOS SANTOS <erik_rafael-santos@estudante.sesisenai.org.br>
2026-03-28 13:45:52 -0300

    feat: testing

---
+++ src/__tests__/unit/example.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+
+describe('Exemplo de teste', () => {
+  it('deve funcionar', () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ vitest.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vitest/config';
+import path from 'path';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./src/test/setup.ts'],
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    exclude: ['**/node_modules/**', '**/e2e/**', '**/tests/e2e/**'],
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
